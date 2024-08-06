c80485e18c98e6147b950473cddbe0469905ca91
Cody Spratford <koreanpanda345@gmail.com>
2024-08-06 14:57:11 -0700

    Update format-data.ts

--- data/mods/ttc_base/format-data.ts
+++ data/mods/ttc_base/format-data.ts
@@ -2,106 +2,106 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	palafinhero: {
 		isNonstandard: "Past",
 		tier: "OU",
-		natDexTier: "OU",
+		
 	},
 	sawsbucksummer: {
 		isNonstandard: "Past",
 		tier: "OU",
-		natDexTier: "OU",
+		
 	},
 	typhlosionhisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	qwilfishhisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	sneaselhisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	samurotthisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	lilliganthisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	zoruahisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	zoroarkhisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	braviaryhisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	sliggoohisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	goodrahisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	avalugghisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	decidueyehisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	overqwil: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	growlithehisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	arcaninehisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	voltorbhisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	electrodehisui: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	enamorus: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 	enamorustherian: {
 		isNonstandard: 'Past',
-		natDexTier: "OU",
+		
 		tier: "OU",
 	},
 };
